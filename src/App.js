e183d32650bb020da8a8060ec9693574e58cb47e
Andre Mechali <andre.mechali@maersk.com>
2019-11-30 10:39:56 +0100

    feat: add files for homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Homepage from "./homepage.component";
 import "./App.css";
 
 function App() {
-  return <div>Hello!</div>;
+  return <Homepage />;
 }
 
 export default App;
